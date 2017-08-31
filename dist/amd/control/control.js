06ff764485d752507fc10a08c5c392be06a446a9
bgando <gandolfo.bianca@gmail.com>
2017-08-31 11:21:53 -0400

    3.3.4

---
+++ dist/amd/control/control.js
@@ -0,0 +1,68 @@
+/*can-component@3.3.3#control/control*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-control',
+    'can-util/js/each',
+    'can-util/js/string',
+    'can-compute',
+    'can-stache-key'
+], function (require, exports, module) {
+    var Control = require('can-control');
+    var canEach = require('can-util/js/each');
+    var string = require('can-util/js/string');
+    var canCompute = require('can-compute');
+    var observeReader = require('can-stache-key');
+    var paramReplacer = /\{([^\}]+)\}/g;
+    var ComponentControl = Control.extend({
+        _lookup: function (options) {
+            return [
+                options.scope,
+                options,
+                window
+            ];
+        },
+        _removeDelegateFromKey: function (key) {
+            return key.replace(/^(scope|^viewModel)\./, '');
+        },
+        _isDelegate: function (options, key) {
+            return key === 'scope' || key === 'viewModel';
+        },
+        _getDelegate: function (options, key) {
+            return options[key];
+        },
+        _action: function (methodName, options, controlInstance) {
+            var hasObjectLookup;
+            paramReplacer.lastIndex = 0;
+            hasObjectLookup = paramReplacer.test(methodName);
+            if (!controlInstance && hasObjectLookup) {
+                return;
+            } else {
+                return Control._action.apply(this, arguments);
+            }
+        }
+    }, {
+        setup: function (el, options) {
+            this.scope = options.scope;
+            this.viewModel = options.viewModel;
+            return Control.prototype.setup.call(this, el, options);
+        },
+        off: function () {
+            if (this._bindings) {
+                canEach(this._bindings.readyComputes || {}, function (value) {
+                    value.compute.unbind('change', value.handler);
+                });
+            }
+            Control.prototype.off.apply(this, arguments);
+            this._bindings.readyComputes = {};
+        },
+        destroy: function () {
+            Control.prototype.destroy.apply(this, arguments);
+            if (typeof this.options.destroy === 'function') {
+                this.options.destroy.apply(this, arguments);
+            }
+        }
+    });
+    module.exports = ComponentControl;
+});
